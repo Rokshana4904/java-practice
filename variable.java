1d98126f39127ea1814afc629741b76d0b7bcbea
Rokshana Talukder Ratri <talukder15-4904@diu.edu.bd>
2026-03-10 21:28:47 +0600

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,31 @@
+
+public class variable {
+	public static void main(String args[])
+	{
+		int a = 10;
+		System.out.println("a: " +a);	
+		String name = "John";
+		System.out.println("Name: " +name);
+		float b = 5.99f;
+		System.out.println("b: " +b);
+		char c = 'C';
+		System.out.println("c: " +c);
+		boolean d = true;
+		System.out.println("d: " +d);
+		String name2 = "John ";
+		String name3 = "Doe";
+		String name4 = name2 + name3;
+		System.out.println("Name: " +name4);
+		int x = 5;
+		int y = 6;
+		System.out.println("The sum of x and y is: " + (x + y));
+		int t = 5;
+		int u = 6;
+		int i = 50;
+		System.out.println(t+u+i);
+		int q, e, r;
+		q = e = r = 50;
+		System.out.println(q + e+ r);
+	}
+
+}
