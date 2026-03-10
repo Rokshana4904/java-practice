a32b5f0627f338d48f70b2d3e829b707d381917e
Rokshana Talukder Ratri <talukder15-4904@diu.edu.bd>
2026-03-10 20:52:52 +0600

    Add files via upload
    Practice Java

---
+++ sum.java
@@ -0,0 +1,11 @@
+
+public class sum {
+	public static void main(String[]args) {
+		System.out.println(10+20);
+		System.out.println(20+30);
+		System.out.println(50+60);
+		System.out.println(40+50);
+		System.out.println(60+70);
+	}
+}
+
