f09278d3050635c9b18854edc65e02e76176f500
Rokshana Talukder Ratri <talukder15-4904@diu.edu.bd>
2026-03-15 21:40:08 +0600

    java practice for For each loop

---
+++ for_each_loop.java
@@ -0,0 +1,19 @@
+
+public class for_each_loop {
+	public static void main(String args[]) {
+		//Problem01
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for(String car : cars) {
+			System.out.println(car);
+		}
+		System.out.println();
+		
+		//Problem02
+		int[] numbers = {10, 20, 30, 40};
+
+		for (int num : numbers) {
+		  System.out.println(num);
+		}
+	}
+
+}
